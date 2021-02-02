b023ade06759c34eecef6f1ebf9297c9cc66e033
André Dias <andremssdias@hotmail.com>
2021-02-02 01:40:37 +0100

    menu done + scrapped 50 pages of books and created generator

--- scraping_books/app.py
+++ scraping_books/app.py
@@ -1,9 +1,12 @@
 import requests
 from pages.all_books_page import AllBooksPage
 
-page_content = requests.get('https://books.toscrape.com/').content
+books = []
 
-book_page = AllBooksPage(page_content)
+for page_num in range(1, 50):
+    url = f'https://books.toscrape.com/catalogue/page-{page_num}.html'
+    page_content = requests.get(url).content
+    page = AllBooksPage(page_content)
+    books.append(page.books)
 
-for book in book_page.books:
-    print(book)
+print(books)
