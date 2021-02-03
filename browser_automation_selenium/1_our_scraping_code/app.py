b4e25dd4875612874ce92b3b16a0f75fcd0f96b7
André Dias <andremssdias@hotmail.com>
2021-02-03 01:16:37 +0100

    User interaction thru website to get quotes

--- browser_automation_selenium/1_our_scraping_code/app.py
+++ browser_automation_selenium/1_our_scraping_code/app.py
@@ -1,9 +1,31 @@
-import requests
+from selenium import webdriver
 
 from pages.quotes_page import QuotesPage
 
-page_content = requests.get('http://quotes.toscrape.com').content
-page = QuotesPage(page_content)
+try:
+    author = input("Enter the author you'd like quotes from: ")
+    tag = input('Enter your tag: ')
 
-for quote in page.quotes:
-    print(quote)
+    chrome = webdriver.Chrome(executable_path=r'C:\Users\André\Desktop\Programming\chromedriver.exe')
+    chrome.get('http://quotes.toscrape.com/search.aspx')
+    page = QuotesPage(chrome)
+
+
+    print(page.search_for_quotes(author,tag))
+
+except InvalidTagForAuthorError as e:
+    print(e)
+except Exception as e:
+    print(e)
+    print("An unknown error occurred. Please try again.")
+
+
+
+
+# page.select_author(author)
+# tags = page.get_available_tags()
+# print('Select one of these tags: [{}]'.format(" | ".join(tags)))
+# page.select_tag(selected_tag)
+# page.search_button.click()
+
+# print(page.quotes)
